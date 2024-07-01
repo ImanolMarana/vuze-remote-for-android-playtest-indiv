869842e61007015ba1297abf1040abf3b9a83c13
ImanolMarana <150240625+ImanolMarana@users.noreply.github.com>
2024-07-01 18:49:48 +0200

    Refactoring id#12

--- vuzeAndroidRemote/src/main/java/com/vuze/android/remote/rpc/RPC.java
+++ vuzeAndroidRemote/src/main/java/com/vuze/android/remote/rpc/RPC.java
@@ -42,111 +42,119 @@ import android.util.Log;
 
 public class RPC
 {
-	private static final String URL_PAIR = "https://pair.vuze.com/pairing/remote";
+        private static final String URL_PAIR = "https://pair.vuze.com/pairing/remote";
 
-	private static final String TAG = "RPC";
+        private static final String TAG = "RPC";
 
-	@SuppressWarnings("rawtypes")
-	public static Map getBindingInfo(String ac, RemoteProfile remoteProfile)
-			throws RPCException {
-		String url = URL_PAIR + "/getBinding?sid=xmwebui&ac=" + ac;
-		try {
-			RestJsonClient restJsonClient = RestJsonClient.getInstance(false, false);
-			Object map = restJsonClient.connect(url);
-			if (map instanceof Map) {
-				//System.out.println("is map");
-				Object result = ((Map) map).get("result");
-				if (result instanceof Map) {
-					//System.out.println("result is map");
-					return (Map) result;
-				} else {
-					return (Map) map;
-				}
-			}
+        @SuppressWarnings("rawtypes")
+        public static Map getBindingInfo(String ac, RemoteProfile remoteProfile)
+                        throws RPCException {
+                String url = URL_PAIR + "/getBinding?sid=xmwebui&ac=" + ac;
+                try {
+                        RestJsonClient restJsonClient = RestJsonClient.getInstance(false, false);
+                        Object map = restJsonClient.connect(url);
+                        if (map instanceof Map) {
+                                return extractBindingResult((Map) map);
+                        }
 
-			if (AndroidUtils.DEBUG_RPC) {
-				Log.d(TAG, "getBindingInfo: empty or invalid reply from pair rpc");
-			}
+                        if (AndroidUtils.DEBUG_RPC) {
+                                Log.d(TAG, "getBindingInfo: empty or invalid reply from pair rpc");
+                        }
 
-			if (remoteProfile != null) {
-				Map lastBindingInfo = remoteProfile.getLastBindingInfo();
-				if (lastBindingInfo != null && lastBindingInfo.size() >= 3) {
-					if (AndroidUtils.DEBUG_RPC) {
-						Log.d(TAG, "getBindingInfo: using last bindingInfo");
-					}
-					return lastBindingInfo;
-				}
-			}
-		} catch (RPCException e) {
-			if (remoteProfile != null) {
-				Map lastBindingInfo = remoteProfile.getLastBindingInfo();
-				if (lastBindingInfo != null && lastBindingInfo.size() >= 3) {
-					if (AndroidUtils.DEBUG_RPC) {
-						Log.d(TAG, "getBindingInfo: using last bindingInfo");
-					}
-					return lastBindingInfo;
-				}
-			}
-			throw e;
-		}
-		return Collections.EMPTY_MAP;
-	}
+                        return handleLastBindingInfo(remoteProfile);
+                } catch (RPCException e) {
+                        return handleLastBindingInfo(remoteProfile, e);
+                }
+        }
 
-	public static boolean isLocalAvailable() {
-		Object oldThreadPolicy = null;
-		try {
-			if (android.os.Build.VERSION.SDK_INT > 9) {
-				// allow synchronous networking because we are only going to localhost
-				// and it will return really fast (it better!)
-				oldThreadPolicy = enableNasty();
-			}
+        @SuppressWarnings("rawtypes")
+        private static Map extractBindingResult(Map map) {
+                Object result = map.get("result");
+                if (result instanceof Map) {
+                        return (Map) result;
+                } else {
+                        return map;
+                }
+        }
 
-			String url = "http://localhost:9091/transmission/rpc?json="
-					+ URLEncoder.encode("{\"method\":\"session-get\"}", "utf-8");
+        private static Map handleLastBindingInfo(RemoteProfile remoteProfile) {
+                return handleLastBindingInfo(remoteProfile, null);
+        }
 
-			BasicHttpParams basicHttpParams = new BasicHttpParams();
-			HttpProtocolParams.setUserAgent(basicHttpParams,
-				AndroidUtils.VUZE_REMOTE_USERAGENT);
-			HttpConnectionParams.setConnectionTimeout(basicHttpParams, 200);
-			HttpConnectionParams.setSoTimeout(basicHttpParams, 900);
-			HttpClient httpclient = new DefaultHttpClient(basicHttpParams);
+        private static Map handleLastBindingInfo(RemoteProfile remoteProfile,
+                        RPCException e) throws RPCException {
+                if (remoteProfile != null) {
+                        Map lastBindingInfo = remoteProfile.getLastBindingInfo();
+                        if (lastBindingInfo != null && lastBindingInfo.size() >= 3) {
+                                if (AndroidUtils.DEBUG_RPC) {
+                                        Log.d(TAG, "getBindingInfo: using last bindingInfo");
+                                }
+                                return lastBindingInfo;
+                        }
+                }
+                if (e != null) {
+                        throw e;
+                }
+                return Collections.EMPTY_MAP;
+        }
 
-			// Prepare a request object
-			HttpGet httpget = new HttpGet(url); // IllegalArgumentException
+//Refactoring end
 
-			// Execute the request
-			HttpResponse response = httpclient.execute(httpget);
+        public static boolean isLocalAvailable() {
+                Object oldThreadPolicy = null;
+                try {
+                        if (android.os.Build.VERSION.SDK_INT > 9) {
+                                // allow synchronous networking because we are only going to localhost
+                                // and it will return really fast (it better!)
+                                oldThreadPolicy = enableNasty();
+                        }
 
-			if (response.getStatusLine().getStatusCode() == 409) {
-				// Must be RPC!
-				return true;
-			}
+                        String url = "http://localhost:9091/transmission/rpc?json="
+                                        + URLEncoder.encode("{\"method\":\"session-get\"}", "utf-8");
 
-		} catch (HttpHostConnectException ignore) {
-			// Connection to http://localhost:9091 refused
-		} catch (Throwable e) {
-			Log.e("RPC", "isLocalAvailable", e);
-		} finally {
-			if (android.os.Build.VERSION.SDK_INT > 9) {
-				revertNasty((ThreadPolicy) oldThreadPolicy);
-			}
-		}
-		return false;
-	}
+                        BasicHttpParams basicHttpParams = new BasicHttpParams();
+                        HttpProtocolParams.setUserAgent(basicHttpParams,
+                                AndroidUtils.VUZE_REMOTE_USERAGENT);
+                        HttpConnectionParams.setConnectionTimeout(basicHttpParams, 200);
+                        HttpConnectionParams.setSoTimeout(basicHttpParams, 900);
+                        HttpClient httpclient = new DefaultHttpClient(basicHttpParams);
 
-	@TargetApi(Build.VERSION_CODES.GINGERBREAD)
-	private static Object enableNasty() {
-		ThreadPolicy oldThreadPolicy = StrictMode.getThreadPolicy();
-		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitNetwork().build();
-		StrictMode.setThreadPolicy(policy);
-		return oldThreadPolicy;
-	}
+                        // Prepare a request object
+                        HttpGet httpget = new HttpGet(url); // IllegalArgumentException
 
-	@TargetApi(Build.VERSION_CODES.GINGERBREAD)
-	private static void revertNasty(@Nullable ThreadPolicy oldPolicy) {
-		if (oldPolicy == null) {
-			return;
-		}
-		StrictMode.setThreadPolicy(oldPolicy);
-	}
+                        // Execute the request
+                        HttpResponse response = httpclient.execute(httpget);
+
+                        if (response.getStatusLine().getStatusCode() == 409) {
+                                // Must be RPC!
+                                return true;
+                        }
+
+                } catch (HttpHostConnectException ignore) {
+                        // Connection to http://localhost:9091 refused
+                } catch (Throwable e) {
+                        Log.e("RPC", "isLocalAvailable", e);
+                } finally {
+                        if (android.os.Build.VERSION.SDK_INT > 9) {
+                                revertNasty((ThreadPolicy) oldThreadPolicy);
+                        }
+                }
+                return false;
+        }
+
+        @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+        private static Object enableNasty() {
+                ThreadPolicy oldThreadPolicy = StrictMode.getThreadPolicy();
+                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitNetwork().build();
+                StrictMode.setThreadPolicy(policy);
+                return oldThreadPolicy;
+        }
+
+        @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+        private static void revertNasty(@Nullable ThreadPolicy oldPolicy) {
+                if (oldPolicy == null) {
+                        return;
+                }
+                StrictMode.setThreadPolicy(oldPolicy);
+        }
 }
